2bfd0cb692ea43a2b13bde0c531bdcb1728ef366
dgupta23 <dgupta23@ncsu.edu>
2023-04-02 22:59:34 -0400

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import mqtt from "mqtt";
 
 function App() {
   const [doorStatus, setDoorStatus] = useState(null);
-
+  // const eventSource = new EventSource('http://localhost:8000/sse/message/');
+  // console.log("djdjd");
   useEffect(() => {
-    const eventSource = new EventSource('http://localhost:8000/sse/stream/my-sse-stream');
+      // eventSource.onmessage = (event) => {
+      // const data = JSON.parse(event.data);
+      // console.log("heys");
+      // setDoorStatus(data);
+      const client = mqtt.connect('http://broker.hivemq.com', {
+      clientId: 'clientId-12345',
+    });
 
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      setDoorStatus(data.status);
-    };
+    client.on('connect', () => {
+      console.log('connected to mqtt broker');
+      client.subscribe('iot/g1/door/status');
+    });
+
+    client.on('message', (topic, message) => {
+      console.log('received message:', message.toString());
+      setDoorStatus(message.toString());
+    });
   }, []);
 
   return (
     <div className="App App-header">
+      {/* {console.log(doorStatus)} */}
       <h2>Door Status : {doorStatus || 'Loading...'}</h2>
     </div>
   );
